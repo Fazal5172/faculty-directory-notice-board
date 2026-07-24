b1d7c09299c8cb8c1e9e0c4ee9dca2ce2de9b240
Fazal <fazalabbas5172@gmail.com>
2026-07-24 22:43:40 +0500

    Updated bootstrap file

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -1,8 +1,5 @@
 <?php
-/**
- * Bootstrap
- * Load config and all classes. Include this at the top of every PHP page.
- */
+
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../classes/Database.php';
 require_once __DIR__ . '/../classes/Auth.php';
